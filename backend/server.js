14960f84b3345a3ac21e0c5a3aade4a8df014f75
crisluda <crisluda2244@gmail.com>
2021-02-24 20:58:22 +0000

    still working on the backend

--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,17 @@ const express = require('express');
 const products = require('./data/products');
 const { report } = require('process');
  const app=express()
+
 app.get("/",(req,res)=>{
-res.send(`hoooooo`)
-})
+    res.send(`hoooooo`)
+    })
 app.get("/api/products",(req,res)=>{
     res.json(products)
     })
-    app.get("/api/products/:id",(req,res)=>{
-        const product= products.find(p=>p._id===req.params.id)
-        res.json(product)
-        })   
+app.get("/api/products/:id",(req,res)=>{
+    const product= products.find(p=>p._id===req.params.id)
+    res.json(product)
+     })   
     
 
 
