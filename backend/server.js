b397b862440a3ae75319f474fdd00ee3733dfeb1
crisluda <crisluda2244@gmail.com>
2021-02-27 22:48:13 +0000

    move the product routes to productRoutes

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import products from './data/products.js';
+// import products from './data/products.js';
 import dotenv from 'dotenv'
 import connectDB from './config/db.js';
 import colors from 'colors';
+import prouductRoutes from './routes/productRoutes.js';
+import {notFound,errorHandler} from './middleware/errorMiddleware.js';
 dotenv.config()
 connectDB()
  const app=express()
@@ -10,15 +12,12 @@ connectDB()
 app.get("/",(req,res)=>{
     res.send(`hoooooo`)
     })
-app.get("/api/products",(req,res)=>{
-    res.json(products)
-    })
-app.get("/api/products/:id",(req,res)=>{
-    const product= products.find(p=>p._id===req.params.id)
-    res.json(product)
-     })   
-    
+  
+    app.use("/api/products",prouductRoutes)
 
+    app.use(notFound)
+
+    app.use(errorHandler)
 
 
 
